ce142fc00d7ca445c523a56894700b29a26bb64f
dongheeoh <donghee1266@naver.com>
2019-05-15 10:45:40 +0900

    html 사이즈

--- src/option/html.js
+++ src/option/html.js
@@ -1,9 +1,9 @@
 export function html() {
     let a = 
-    "<div class='p_docmanageroot'><table width='700' style='width: 550pt; border-collapse: collapse;' border='0' cellspacing='0' cellpadding='0'>"+
+    "<div class='p_docmanageroot'><table width='700' style='width: 720pt; border-collapse: collapse;' border='0' cellspacing='0' cellpadding='0'>"+
     "<colgroup><col width='160' style='width: 120pt; mso-width-source: userset; mso-width-alt: 5120;'>"+
     "<col width='680' style='width: 510pt; mso-width-source: userset; mso-width-alt: 21760;'>"+
-    "<col width='118' style='width: 89pt; mso-width-source: userset; mso-width-alt: 3776;'>"+
+    "<col width='118' style='width: 90pt; mso-width-source: userset; mso-width-alt: 3776;'>"+
     "</colgroup><tbody><tr height='22' style='height: 16.5pt;'>"+
      "<td width='958' height='22' class='xl83' style='width: 719pt; height: 22px; text-align: center; font-size: 25pt; font-weight: 700;' colspan='3'>일일업무일지</td>"+
     "</tr>"+
@@ -13,10 +13,10 @@ export function html() {
     "<td height='22' class='xl66' style='background: rgb(217, 217, 217); border: 0.75pt solid windowtext; border-image: none; height: 16.5pt; text-align: center; font-size: 16pt; font-weight: 700;'><b>오늘의<br>주요업무</b></td>"+
     "<td width='680' class='xl67' style='background: rgb(217, 217, 217); border: 0.75pt solid windowtext; border-image: none; width: 510pt; text-align: center; font-size: 9pt; font-weight: 700; white-space: normal;' colspan='2'></td>"+
     "<tr height='48' style='height: 36pt;'>"+
-        "<td height='22' class='xl66' style='background: rgb(217, 217, 217); border: 0.75pt solid windowtext; border-image: none; height: 16.5pt; text-align: center; font-size: 16pt; font-weight: 700;' colspan='4' ><b>세부업무내용</b></td>"+
+        "<td height='22' class='xl66' style='background: rgb(217, 217, 217); border: 0.75pt solid windowtext; border-image: none; height: 16.5pt; text-align: center; font-size: 16pt; font-weight: 700;' colspan='3' ><b>세부업무내용</b></td>"+
       "<tr height='48' style='height: 36pt;'>"+
-     "<td width='680' class='xl77' style='border: 0.75pt solid windowtext; border-image: none; width: 510pt; color: windowtext; font-size: 16pt; white-space: normal; text-align: center;' colspan='2'><b>업무내용</b></td>"+
-     "<td class='xl69' style='border: 0.75pt solid windowtext; border-image: none; font-size: 16pt; text-align: center;'><b>비고</b></td>"+
+     "<td width='680' class='xl66' style='border: 0.75pt solid windowtext; border-image: none; width: 510pt; color: windowtext; font-size: 16pt; white-space: normal; text-align: center;' colspan='2'><b>업무내용</b></td>"+
+     "<td class='xl66' style='border: 0.75pt solid windowtext; border-image: none; font-size: 16pt; text-align: center;'><b>비고</b></td>"+
     "</tr>"+
     "<tr height='48' style='height: 36pt;'>"+
     "<td width='680' class='xl77' style='border: 0.75pt solid windowtext; border-image: none; width: 510pt; color: windowtext; font-size: 9pt; white-space: normal;' colspan='2'></td>"+
@@ -54,10 +54,10 @@ export function html() {
 };
 export function html2() {
     let a =
-        "<div class='p_docmanageroot'><table width='958' style='width: 719pt; border-collapse: collapse;' border='0' cellspacing='0' cellpadding='0'>"+
-        "<colgroup><col width='160' style='width: 120pt; mso-width-source: userset; mso-width-alt: 5120;'>"+
-        "<col width='80' style='width: 70pt; mso-width-source: userset; mso-width-alt: 3776;'>"+
-        "<col width='700' style='width: 700pt; mso-width-source: userset; mso-width-alt: 21760;'>"+
+        "<div class='p_docmanageroot'><table width='700' style='width: 720pt; border-collapse: collapse;' border='0' cellspacing='0' cellpadding='0'>"+
+        "<colgroup><col width='160' style='width: 80pt; mso-width-source: userset; mso-width-alt: 5120;'>"+
+        "<col width='80' style='width: 50pt; mso-width-source: userset; mso-width-alt: 3776;'>"+
+        "<col width='700' style='width: 590pt; mso-width-source: userset; mso-width-alt: 21760;'>"+
         "</colgroup><tbody><tr height='22' style='height: 16.5pt;'>"+
          "<td width='958' height='22' class='xl83' style='width: 719pt; height: 22px; text-align: center; font-size: 25pt; font-weight: 700;' colspan='3'>주간업무일지</td>"+
         "</tr>"+
@@ -68,11 +68,11 @@ export function html2() {
         "<td width='680' class='xl67' style='background: rgb(217, 217, 217); border: 0.75pt solid windowtext; border-image: none; width: 510pt; text-align: center; font-size: 16pt; font-weight: 700; white-space: normal;' colspan='2'><b>업무내용</b></td></tr>"+
         "<tr height='48' style='height: 100pt;'>"+
             "<td height='22' class='xl66' style='background: rgb(217, 217, 217); border: 0.75pt solid windowtext; border-image: none; height: 16.5pt; text-align: center; font-size: 16pt; font-weight: 700;'  ><b>지난주<br>결과보고</b></td>"+
-            "<td height='22' class='xl66' style='border: 0.75pt solid windowtext; border-image: none; height: 16.5pt; text-align: center; font-size: 9pt; font-weight: 700;' colspan='3' ></td>"+
+            "<td height='22' class='xl66' style='border: 0.75pt solid windowtext; border-image: none; height: 16.5pt; text-align: center; font-size: 9pt; font-weight: 700;' colspan='2' ></td>"+
         "</tr>"+
          "<tr height='48' style='height: 100pt;'>"+
             "<td height='22' class='xl66' style='background: rgb(217, 217, 217); border: 0.75pt solid windowtext; border-image: none; height: 16.5pt; text-align: center; font-size: 16pt; font-weight: 700;'  ><b>다음주<br>업무계획</b></td>"+
-            "<td height='22' class='xl66' style=' border: 0.75pt solid windowtext; border-image: none; height: 16.5pt; text-align: center; font-size: 9pt; font-weight: 700;' colspan='3' ></td>"+
+            "<td height='22' class='xl66' style=' border: 0.75pt solid windowtext; border-image: none; height: 16.5pt; text-align: center; font-size: 9pt; font-weight: 700;' colspan='2' ></td>"+
         "</tr>"+
           "<tr height='48' style='height: 50pt; '>"+
          "<td width='680' class='xl77' style='background: rgb(217, 217, 217); border: 0.75pt solid windowtext; border-image: none; width: 100pt; color: windowtext; font-size: 16pt; white-space: normal; text-align: center;' rowspan='6'><b>세부<br>업무계획</b></td>"+
@@ -110,13 +110,13 @@ export function html2() {
 
 export function html3() {
     let a =
-    "<div class='p_docmanageroot'><table width='960' style='width: 719pt; border-collapse: collapse;' border='0' cellspacing='0' cellpadding='0'>"+
+    "<div class='p_docmanageroot'><table width='960' style='width: 720pt; border-collapse: collapse;' border='0' cellspacing='0' cellpadding='0'>"+
     "<colgroup>"+
-    "<col width='192' style='width: 100pt; mso-width-source: userset; mso-width-alt: 5120;'>"+
-    "<col width='192' style='width: 350pt; mso-width-source: userset; mso-width-alt: 21760;'>"+
-    "<col width='192' style='width: 150pt; mso-width-source: userset; mso-width-alt: 3776;'>"+
-    "<col width='192' style='width: 150pt; mso-width-source: userset; mso-width-alt: 3776;'>"+
-    "<col width='192' style='width: 89pt; mso-width-source: userset; mso-width-alt: 3776;'>"+
+    "<col width='192' style='width: 60pt; mso-width-source: userset; mso-width-alt: 5120;'>"+
+    "<col width='192' style='width: 330pt; mso-width-source: userset; mso-width-alt: 21760;'>"+
+    "<col width='192' style='width: 140pt; mso-width-source: userset; mso-width-alt: 3776;'>"+
+    "<col width='192' style='width: 140pt; mso-width-source: userset; mso-width-alt: 3776;'>"+
+    "<col width='192' style='width: 50pt; mso-width-source: userset; mso-width-alt: 3776;'>"+
     "</colgroup><tbody>"+
     "<tr height='22' style='height: 16.5pt;'>"+
      "<td width='958' height='22' class='xl83' style='width: 719pt; height: 22px; text-align: center; font-size: 25pt; font-weight: 700;' colspan='5'>월간업무일지</td>"+
@@ -124,14 +124,14 @@ export function html3() {
     "<tr height='22' style='height: 16.5pt;'>"+
     "</tr>"+
     "<tr height='22' style='height: 25pt;'>"+
-    "<td height='22' class='xl66' style='background: rgb(217, 217, 217); border: 0.75pt solid windowtext; border-image: none; height: 16.5pt; font-size: 12pt; font-weight: 700;' colspan='5'><b> ● 당월 중요 업무내역</b></td>"+
+    "<td height='22' class='xl66' style='background: rgb(217, 217, 217); border: 0.75pt solid windowtext; border-image: none; height: 16.5pt; font-size: 12pt; font-weight: 700;' colspan='5'><b>● 당월 중요 업무내역</b></td>"+
     "</tr>"+
     "<tr height='48' style='height: 80pt;'>"+
-        "<td height='22' class='xl66' style='background: rgb(217, 217, 217); border: 0.75pt solid windowtext; border-image: none; height: 16.5pt; text-align: center; font-size: 16pt; font-weight: 700;'  ><b>업무내용</b></td>"+
+        "<td height='22' class='xl66' style='background: rgb(217, 217, 217); border: 0.75pt solid windowtext; border-image: none; height: 16.5pt; text-align: center; font-size: 12pt; font-weight: 700;'><b>업무내용</b></td>"+
          "<td height='22' class='xl66' style='border: 0.75pt solid windowtext; border-image: none; height: 16.5pt; text-align: center; font-size: 16pt; font-weight: 700;' colspan='4'><b></b></td>"+
      "</tr>"+
      "<tr height='48' style='height: 80pt;'>"+
-        "<td height='22' class='xl66' style='background: rgb(217, 217, 217); border: 0.75pt solid windowtext; border-image: none; height: 16.5pt; text-align: center; font-size: 16pt; font-weight: 700;'  ><b>결과/산출물</b></td>"+
+        "<td height='22' class='xl66' style='background: rgb(217, 217, 217); border: 0.75pt solid windowtext; border-image: none; height: 16.5pt; text-align: center; font-size: 12pt; font-weight: 700;'><b>결과/<br>산출물</b></td>"+
          "<td height='22' class='xl66' style='border: 0.75pt solid windowtext; border-image: none; height: 16.5pt; text-align: center; font-size: 16pt; font-weight: 700;' colspan='4'><b></b></td>"+
      "</tr>"+
      "<tr height='22' style='height: 25pt;'>"+
@@ -140,12 +140,12 @@ export function html3() {
     "<tr height='48' style='height: 36pt;'>"+
      "<td width='680' class='xl77' style='background: rgb(217, 217, 217); border: 0.75pt solid windowtext; border-image: none; width: 510pt; color: windowtext; font-size: 16pt; white-space: normal; text-align: center;' colspan='5'><b>추진내역</b></td>"+
     "</tr>"+
-    "<tr height='48' style='height: 36pt; text-align: center; background: rgb(217, 217, 217);'>"+
-     "<td class='xl69' style='border: 0.75pt solid windowtext; border-image: none; font-size: 11pt;'><b>주차</b></td>"+
-     "<td class='xl69' style='border: 0.75pt solid windowtext; border-image: none; font-size: 11pt;'><b>실천업무</b></td>"+
-     "<td class='xl69' style='border: 0.75pt solid windowtext; border-image: none; font-size: 11pt;'><b>중점확인사항</b></td>"+
-     "<td class='xl69' style='border: 0.75pt solid windowtext; border-image: none; font-size: 11pt;'><b>결과</b></td>"+
-     "<td class='xl69' style='border: 0.75pt solid windowtext; border-image: none; font-size: 11pt;'><b>비고</b></td>"+
+    "<tr height='48' style='height: 36pt; text-align:center; background: rgb(217, 217, 217);'>"+
+     "<td class='xl69' style='text-align:center; border: 0.75pt solid windowtext; border-image: none; font-size: 11pt;'><b>주차</b></td>"+
+     "<td class='xl69' style='text-align:center; border: 0.75pt solid windowtext; border-image: none; font-size: 11pt;'><b>실천업무</b></td>"+
+     "<td class='xl69' style='text-align:center; border: 0.75pt solid windowtext; border-image: none; font-size: 11pt;'><b>중점확인사항</b></td>"+
+     "<td class='xl69' style='text-align:center; border: 0.75pt solid windowtext; border-image: none; font-size: 11pt;'><b>결과</b></td>"+
+     "<td class='xl69' style='text-align:center; border: 0.75pt solid windowtext; border-image: none; font-size: 11pt;'><b>비고</b></td>"+
     "</tr>"+
     "<tr height='48' style='height: 80pt; '>"+
      "<td class='xl69' style='text-align: center; background: rgb(217, 217, 217); border: 0.75pt solid windowtext; border-image: none; font-size: 12pt;'>"+
